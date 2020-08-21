536047a41b87779d2706daba9f177d2ffc0120dd
dhflxhdxhd@gmail.com <dhflxhdxhd@gmail.com>
2020-08-21 10:58:43 +0900

    clone_momentum

--- clone_momentum/clone_clock.js
+++ clone_momentum/clone_clock.js
@@ -1,16 +1,20 @@
 const clockContainer = document.querySelector(".clock-js")
 , clockTitle = document.querySelector(".title-js");
 
+
 function getTime() {
     const date = new Date();
     const mimutes = date.getMinutes();
     const hours = date.getHours();
     const sec = date.getSeconds();
-    clockTitle.innerHTML = `${hours}:${mimutes}:${sec}`;
+    clockTitle.innerHTML = 
+    `${hours <10 ? `0${hours}` : `${hours}`}:${mimutes < 10 ? `0${mimutes}`:`${mimutes}`}:${sec<10 ? `0${sec}` : `${sec}`}`;
 }
 
+
+
 function init() {
-    getTime();
+    setInterval(getTime,1000);
 }
 
 
---
+++ clone_momentum/greeting.js
@@ -0,0 +1,46 @@
+const form = document.querySelector(".js-form"),
+input = form.querySelector("input")
+, greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "currentUser",
+SHOWING_CN = "showing";  //showing의 정체는 무엇인가.??
+
+function savename(text){
+    localStorage.setItem(USER_LS,text);
+}
+
+function handleSubmit(event) {
+    // preventDefault : event 금지.
+    // 기본 동작을 막는데 필요한 step 1.
+    event.preventDefault();
+    const currentValue = input.value;
+    // console.log(currentValue);
+    paintGreeting(currentValue);
+    savename(currentValue);
+}
+
+function askforname(){
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+}
+
+function paintGreeting(text){
+    form.classList.remove(SHOWING_CN);
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerHTML = `Hello ${text}`
+}
+
+function loadName() {
+    const currentUser = localStorage.getItem(USER_LS);
+    if (currentUser === null){
+        askforname();
+    }else {
+        paintGreeting(currentUser);
+    }
+}   
+
+function init(){
+    loadName();
+}
+
+init();
