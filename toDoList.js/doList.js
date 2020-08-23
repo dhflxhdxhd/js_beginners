15ca6f904068ac526b753160a9342d013f27021d
dhflxhdxhd@gmail.com <dhflxhdxhd@gmail.com>
2020-08-23 23:18:25 +0900

    doList modify

--- toDoList.js/doList.js
+++ toDoList.js/doList.js
@@ -3,15 +3,32 @@ const ques = document.querySelector(".js-ques"),
     asList = document.querySelector(".js-doList");
 
 const doList_LS = "dos";
-const dos = [];
+let dos = [];
 
 function saveDos(){
     localStorage.setItem(doList_LS,JSON.stringify(dos));
 }
 
-function delList() {
-    
+function delList(event) {
+    //html에서 delete
+    const btn = event.target;
+    const li = btn.parentNode;
+    // console.log(event.target.parentNode);
+    asList.removeChild(li);
 
+    // LS에서 delete -> filter사용
+
+   
+    const cleanDos = dos.filter(
+        function(toDo){
+            // console.log(toDo.id, li.id);
+            return toDo.id !== parseInt(li.id);
+        });
+    // console.log(cleanDos);
+    // console.log(dos);
+    dos = cleanDos;
+    // console.log(dos);
+    saveDos();
 }
 
 function paintList(text){
@@ -49,14 +66,20 @@ function handleList(event){
 function doListLoad() {
     const toDoList = localStorage.getItem(doList_LS);
     if(toDoList !== null){
+
+        
         // parse : 가져온 것을 js object로 변경
+        // console.log(toDoList);
         const parsedToDos = JSON.parse(toDoList);
+        // console.log(parsedToDos);
         // array를 위한 function
         parsedToDos.forEach(
             function(toDo){
                 paintList(toDo.text);
             }
-         );
+         ); 
+
+        
     }
 }
 
