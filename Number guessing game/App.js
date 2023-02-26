993a61a31964ccec8909811d435d1cd841b91aed
or.llon <dhflxhdxhd@gmail.com>
2023-02-26 22:56:23 +0900

    Feat : form submit 이벤트값 생성

--- Number guessing game/App.js
+++ Number guessing game/App.js
@@ -1,15 +1,21 @@
-const input = document.getElementsByClassName("guessInput");
-
+const input = document.querySelector(".guessInput");
+const guessInput = document.querySelector(".guessInput");
+const guessSubmit = document.querySelector(".guessSubmit");
 const minNum = 1;
 const maxNum = 100;
+let turn = 0;
 
 function makeRandomNumber(){
     return Math.floor(Math.random * maxNum + minNum);
+}
 
+function checkResult(){
+    const userGuess = Number(guessInput.value);
 }
 
 function play(){
     let randomNumber = makeRandomNumber();
+    guessSubmit.addEventListener("click",checkResult);
 }
 
 play();
