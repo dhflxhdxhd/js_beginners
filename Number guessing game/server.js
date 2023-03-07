73d12762b0b4e4573bc14278fb0901510da65e02
or.llon <dhflxhdxhd@gmail.com>
2023-03-07 23:24:14 +0900

    Add : express server

---
+++ Number guessing game/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000;
+
+app.use(express.static('src'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/src/index.html');
+})
+
+app.listen(port, () => {
+    console.log(`server is listening at localhost:${port}`);
+})
